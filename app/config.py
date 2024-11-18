03f1a80e4a3f4997978769479bbba4792abad271
upstagebunion <fco.garcia.solis@gmail.com>
2024-11-18 00:09:05 -0600

    Agregados archivos de deployment de Mysql por consistencia de datos, ajuste a API para rutas GET

--- app/config.py
+++ app/config.py
@@ -1,7 +1,16 @@
 import os
 
+db_user = "user"
+db_password = "userpassword"
+db_host = "mysql-service"
+db_name = "testdb"
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}"
+
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///projects.db")
+    #SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///projects.db")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class TestingConfig(Config):
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,12 @@
 from flask import Blueprint, request, jsonify
 from app.models import db, Student, Project
+from app.schemas import StudentSchema, ProjectSchema
 
 api = Blueprint('api', __name__)
+student_schema = StudentSchema()
+students_schema = StudentSchema(many=True)
+project_schema = ProjectSchema()
+projects_schema = ProjectSchema(many=True)
 
 @api.route('/students', methods=['POST'])
 def create_student():
@@ -18,3 +23,33 @@ def create_project():
     db.session.add(new_project)
     db.session.commit()
     return jsonify({"message": "Proyecto creado con éxito"}), 201
+
+@api.route('/students', methods=['GET'])
+def get_students():
+    students = Student.query.all()
+    students_data = students_schema.dump(students)
+    return jsonify(students_data), 200
+
+# Ruta para obtener un estudiante por id
+@api.route('/students/<int:student_id>', methods=['GET'])
+def get_student(student_id):
+    student = Student.query.get(student_id)
+    if not student:
+        return {"error": "Estudiante no encontrado"}, 404
+    student_data = student_schema.dump(student)
+    return jsonify(student_data), 200
+
+@api.route('/projects', methods=['GET'])
+def get_projects():
+    projects = Project.query.all()
+    projects_data = projects_schema.dump(projects)
+    return jsonify(projects_data), 200
+
+# Ruta para obtener un estudiante por id
+@api.route('/projects/<int:project_id>', methods=['GET'])
+def get_project(project_id):
+    project = Project.query.get(project_id)
+    if not project:
+        return {"error": "Estudiante no encontrado"}, 404
+    project_data = project_schema.dump(project)
+    return jsonify(project_data), 200
